f9c49c14914880f365dafe5f5cce5d0e0d5e4a13
lSebastianFloresl <sebastian.flores301220@gmail.com>
2026-05-26 11:35:15 -0500

    Prueba Render 2

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  console.log("STARTING APP")
+  console.log("1 START")
 
   const app = await NestFactory.create(AppModule)
 
-  console.log("AFTER APP CREATE")
-
-  app.enableCors()
+  console.log("2 AFTER CREATE")
 
   await app.listen(process.env.PORT || 3000)
 
-  console.log("SERVER STARTED")
+  console.log("3 AFTER LISTEN")
 }
+
+bootstrap()
